5eb515e5620f274c1a503613759c4ec7d0a05b5a
MrPotterOP <shubham.ubarhande1044@gmail.com>
2025-07-20 13:14:59 +0530

    Gtag in new way (Gemini method)

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import Chat from "./components/UI/Button/Chat";
-import AnalyticsLoader from "./libs/AnLoader";
+
+import Script from "next/script";
+
 
 const fontPrime = Inter({
   variable: "--font-prime",
@@ -16,13 +18,39 @@ export const metadata = {
   description: "Unify fragmented commerce data in real time with TruFlo. Unlock actionable insights to drive performance and enable profitable omnichannel growth.",
 };
 
+const GTM_ID = process.env.NEXT_PUBLIC_GA_ID;
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager Script */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `,
+          }}
+        />
+      </head>
       <body className={`${fontPrime.variable} font-sans`}>
+        {/* Google Tag Manager NoScript Fallback */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         {children}
         <Chat href="#leadForm" ariaLabel="Open chat" bottom="90px" side="44px" />
-        <AnalyticsLoader />
       </body>
     </html>
   );
